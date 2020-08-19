755729e667b67349b5a0381ebb4aeb9e13ba4eea
davit-isajanyan <david.isajanyan@zoomerang.app>
2020-08-19 16:27:12 +0400

    final fix

--- SJSegmentedScrollView/Classes/SJSegmentView.swift
+++ SJSegmentedScrollView/Classes/SJSegmentView.swift
@@ -424,3 +424,20 @@ class SJSegmentView: UIScrollView {
         }
     }
 }
+
+public class RoundedSelectedView: UIView {
+    public override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        self.toRound()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        self.toRound()
+    }
+    
+    private func toRound() {
+        self.layer.cornerRadius = self.frame.size.height * 0.5
+        self.layer.masksToBounds = true
+    }
+}
